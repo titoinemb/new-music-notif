f9a520edbe0ca124e6d56b239f046c75d6a87e24
titoinemb <mb_titoine@proton.me>
2025-11-28 20:37:25 -0500

    style(directory): adding struct of project

---
+++ launch.sh
@@ -0,0 +1,1 @@
+ruby ./src/main.rb
--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,25 @@
 #!/usr/bin/env ruby
+
+# import modules
+require 'yaml'
+require 'pathname'
+
+# get the relative path of this file
+base_dir = Pathname.new(__FILE__).dirname
+
+# var for get the config file
+file_path = base_dir.join('../config.yml')
+
+# listing the config file
+begin
+  data = YAML.load_file(file_path)['bot_config']
+
+  # verifing if minimum 1 method for notifing new music is activate
+  if data.any? { |_, service| service["actif?"] }
+    
+  else
+    puts "Error: All notification methods are disabled. Please activate at least one method"
+  end
+rescue StandardError => e
+  puts "Error for listing the config file : #{e.message}"
+end
