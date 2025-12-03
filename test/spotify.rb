87d55317b77f52bcb79d42b0cd390d6501294cb2
titoinemb <mb_titoine@proton.me>
2025-12-03 13:42:17 -0500

    test(checking_spotify): add

---
+++ test/spotify.rb
@@ -0,0 +1,6 @@
+# test for get the last title name of soundcloud artist
+require_relative "../src/services/checking/spotify"
+
+resultat = checking_spotify("76Pl0epAMXVXJspaSuz8im", "/usr/bin/librewolf")
+
+puts resultat
