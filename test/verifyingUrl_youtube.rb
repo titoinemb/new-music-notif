2814dff5f7e064d7af9d8e48ce7e37ef618f19a7
titoinemb <mb_titoine@proton.me>
2025-12-03 10:36:46 -0500

    test: add verifyinfUrl_youtube file

---
+++ test/verifyingUrl_youtube.rb
@@ -0,0 +1,14 @@
+require_relative "../src/utils/verifyingUrl/youtube"
+
+puts verifyingUrl_youtube?("https://www.youtube.com/@freezecorleone7615", "/usr/bin/librewolf")                 # true
+puts verifyingUrl_youtube?("https://www.youtube.com/@UCPj63YTEirrOXgCp5xiBYJQ", "/usr/bin/librewolf")           # true
+puts verifyingUrl_youtube?("https://www.youtube.com/channel/UCPj63YTEirrOXgCp5xiBYJQ", "/usr/bin/librewolf")    # true
+puts verifyingUrl_youtube?("https://music.youtube.com/channel/UCPj63YTEirrOXgCp5xiBYJQ", "/usr/bin/librewolf")  # false
+puts verifyingUrl_youtube?("https://youtube.com", "/usr/bin/librewolf")                                         # false
+puts verifyingUrl_youtube?("https://www.youtube.com", "/usr/bin/librewolf")                                     # false
+puts verifyingUrl_youtube?("https://music.youtube.com", "/usr/bin/librewolf")                                   # false
+puts verifyingUrl_youtube?("https://music.youtube.com/channel", "/usr/bin/librewolf")                           # false
+puts verifyingUrl_youtube?("https://www.youtube.com/channel", "/usr/bin/librewolf")                             # false
+puts verifyingUrl_youtube?("https://youtube.com/channel", "/usr/bin/librewolf")                                 # false
+puts verifyingUrl_youtube?("https://music.youtube.com/@UCPj63YTEirrOXgCp5xiBYJQ", "/usr/bin/librewolf")         # false
+puts verifyingUrl_youtube?("https://www.youtube.com/@freezecorleone7615a", "/usr/bin/librewolf")                # false
