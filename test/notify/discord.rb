1cf18901e0834286f7e2f0ce037bef4be3fda4d1
titoinemb <mb_titoine@proton.me>
2025-12-08 21:43:12 -0500

    feat(notify discord): systeme for send message in discord pm

--- test/notify/discord.rb
+++ test/notify/discord.rb
@@ -1,3 +1,3 @@
 require_relative "../../src/services/notify/discord"
 
-notify_discord("1447767313334927402", "MTQ0Nzc2ODU1Njk1NDU4MzA1Mg.GUgoxy.c2iJ4UcWlycgBCKjeXUJoDuBj76LXg2rKBjTaI", "test")
+notify_discord("", "", "test")
